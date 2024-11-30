5ff5217c36eb6bdbe6629c73562a263118c65c23
FlackCode <fl4ckc0de@gmail.com>
2024-11-30 21:35:48 +0200

    Generics in Go

---
+++ cmd/generics/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+//example1
+// func main() {
+// 	var intSlice = []int{1, 2, 3}
+// 	fmt.Println(sumSlice[int](intSlice))
+
+// 	var float32Slice = []float32{1, 2 ,3}
+// 	fmt.Println(sumSlice[float32](float32Slice))
+// }
+
+// func sumSlice[T int | float32 | float64](slice []T) T{ //[type](parameter) returnType{}
+// 	var sum T
+// 	for _, v:= range slice {
+// 		sum += v
+// 	}
+// 	return sum
+// }
+
+//example2
+
+type gasEngine struct {
+	gallons float32
+	mpg float32
+}
+
+type electricEngine struct {
+	kwh float32
+	mpkwh float32
+}
+
+type car [T gasEngine | electricEngine]struct {
+	carMake string
+	carModel string
+	engine T
+}
+
+func main() {
+	var gasCar = car[gasEngine]{
+		carMake: "Honda",
+		carModel: "Civic",
+		engine: gasEngine{
+			gallons: 12.4,
+			mpg: 40,
+		},
+	}
+	var electricCar = car[electricEngine]{
+		carMake: "Tesla",
+		carModel: "Model 3",
+		engine: electricEngine{
+			kwh: 57.5,
+			mpkwh: 4.17,
+		},
+	}
+
+	fmt.Println(gasCar)
+	fmt.Println(electricCar)
+}
